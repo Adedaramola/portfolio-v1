593b08df513517347e4e833650060320155efd17
Adedaramola <paranok2017@gmail.com>
2022-03-01 12:15:08 +0100

    fixed frontend padding

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('body')
 <div>
-    <header class="flex flex-col space-y-4 items-center justify-center py-16">
+    <header class="flex flex-col space-y-4 items-center text-center justify-center px-4 py-16">
         <p class="text-gray-500 text-xl font-medium">Hi there! I'm</p>
         <h1 class="text-6xl font-bold">Adedaramola Adetimehin</h1>
         <p class="text-2xl font-bold">Software Developer - Web 2 & 3</p>
@@ -10,7 +10,7 @@
 
     <section id="about" class="grid lg:grid-cols-2 border-t-2 border-b-2 border-black divide-x-2 divide-black">
         <div class="bg-black">j</div>
-        <div class="p-12 text-lg">
+        <div class="px-4 py-10 md:p-12 text-lg">
             <h1 class="text-4xl font-bold mb-4">A bit 'bout me</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam iusto sunt architecto voluptate ducimus!
                 Quos molestiae debitis perspiciatis nobis soluta ab. Et nam provident optio qui eaque assumenda illo
